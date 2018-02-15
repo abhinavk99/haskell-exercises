da4d5bf0802ca4e5f744f1a950977f7b24e4125e
abhinavk99 <abhinavkasamsetty@gmail.com>
2018-02-14 18:03:41 -0600

    Learned pattern matching

---
+++ patternmatching.hs
@@ -0,0 +1,19 @@
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
